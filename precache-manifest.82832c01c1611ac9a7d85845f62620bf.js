270e076fd733f9d3d356153c38a6ebdd538cce81
itland development <development@itlandgroup.com>
2020-04-17 15:02:29 +0200

    Updates

---
+++ precache-manifest.82832c01c1611ac9a7d85845f62620bf.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e54510e4b0f98fb8a5cc4db405db9a2b",
+    "url": "/itland-website/index.html"
+  },
+  {
+    "revision": "4ee93a926cdf3b536ba0",
+    "url": "/itland-website/static/css/2.8a599aea.chunk.css"
+  },
+  {
+    "revision": "ba6d03b87b2cef15067d",
+    "url": "/itland-website/static/css/main.a203e1d5.chunk.css"
+  },
+  {
+    "revision": "4ee93a926cdf3b536ba0",
+    "url": "/itland-website/static/js/2.0da3bf59.chunk.js"
+  },
+  {
+    "revision": "ba6d03b87b2cef15067d",
+    "url": "/itland-website/static/js/main.88d87f41.chunk.js"
+  },
+  {
+    "revision": "89dbd2493947bb6381ec",
+    "url": "/itland-website/static/js/runtime~main.83f4c776.js"
+  },
+  {
+    "revision": "3f51499b046d8b19ce9542535c92b220",
+    "url": "/itland-website/static/media/2.3f51499b.png"
+  },
+  {
+    "revision": "f722794190a7a57488bf382f45913633",
+    "url": "/itland-website/static/media/OUT-SOURCING.f7227941.gif"
+  },
+  {
+    "revision": "c91de858a78002c44426b6250c31b175",
+    "url": "/itland-website/static/media/asya.c91de858.jpg"
+  },
+  {
+    "revision": "a6c28a47ca8f6b31bbc9945d0aae47b6",
+    "url": "/itland-website/static/media/aya.a6c28a47.jpg"
+  },
+  {
+    "revision": "912b1b9fca8e80d392e6a60b5f11b1c2",
+    "url": "/itland-website/static/media/business-english.912b1b9f.gif"
+  },
+  {
+    "revision": "22f457d9f86ea68f565dc6d5e37c4249",
+    "url": "/itland-website/static/media/computer-courses.22f457d9.gif"
+  },
+  {
+    "revision": "449c08a2e31bbc0e2d3877e884526a8e",
+    "url": "/itland-website/static/media/digital-marketing.449c08a2.gif"
+  },
+  {
+    "revision": "fd5e05bc573ab98d6906d2c71b19ff8a",
+    "url": "/itland-website/static/media/english-courses.fd5e05bc.gif"
+  },
+  {
+    "revision": "8fbee902f8b8b63d45e86b0ad455e571",
+    "url": "/itland-website/static/media/english-kids.8fbee902.gif"
+  },
+  {
+    "revision": "fa8a35afbe902eed3efc13d01bb8cfc0",
+    "url": "/itland-website/static/media/lap.fa8a35af.png"
+  },
+  {
+    "revision": "5016a22480f655541e79e7a78c1371f9",
+    "url": "/itland-website/static/media/logoLoginModal.5016a224.png"
+  },
+  {
+    "revision": "149dcf6062467898b74e9e98893a27c7",
+    "url": "/itland-website/static/media/lotfe.149dcf60.jpg"
+  },
+  {
+    "revision": "1ded8a1a4b2cc4dc856c78eecadbd4d1",
+    "url": "/itland-website/static/media/map.1ded8a1a.png"
+  },
+  {
+    "revision": "9aaee15e625d0de4be0ba07335155607",
+    "url": "/itland-website/static/media/marihan.9aaee15e.jpg"
+  },
+  {
+    "revision": "396919abf6f3789dcebabf42bd4b95c6",
+    "url": "/itland-website/static/media/mohamed_logo.396919ab.jpg"
+  },
+  {
+    "revision": "34005e0e9463a9390e20a6203ba4797c",
+    "url": "/itland-website/static/media/mohamedibrahim.34005e0e.jpg"
+  },
+  {
+    "revision": "322258784ca5f4ef5d95036b04e71467",
+    "url": "/itland-website/static/media/osama.32225878.jpg"
+  },
+  {
+    "revision": "5866b71842bcc97914631ca206a50c20",
+    "url": "/itland-website/static/media/slide-bg-1.5866b718.jpg"
+  },
+  {
+    "revision": "63b34bfbf7ce29258f7a15b4db130282",
+    "url": "/itland-website/static/media/video-bg.63b34bfb.jpg"
+  },
+  {
+    "revision": "8ac26a2a3481e5cb341bd7d7da9edf2f",
+    "url": "/itland-website/static/media/work-1.8ac26a2a.jpg"
+  },
+  {
+    "revision": "a5cba78579feade9e37abdca2f7c82c2",
+    "url": "/itland-website/static/media/work-2.a5cba785.jpg"
+  }
+]);
